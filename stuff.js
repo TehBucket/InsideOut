ec88e6556ed353049f2d88dd5286b357d6f122fd
TehBucket <wkoutnik@gmail.com>
2014-04-26 13:26:35 -0400

    semi working collission

--- stuff.js
+++ stuff.js
@@ -29,6 +29,13 @@ var newCircle = function(x,y,r){
 
 var colors = [{a:'#4C968C',b:'#FF700A',c:'#09E4C6',d:'#EFAA78'},{a:'#47716B',b:'#BF6F36',c:'#30AB9A',d:'#B38C71'},{a:'#196257',b:'#A64703',c:'039480',d:'#9B5727'},{a:'#80CBC0',b:'#FF9347',c:'#44F1DA',d:'#F7C29C'},{a:'#94CBC3',b:'#FFB078',c:'#72F1E0',d:'#F7D0B4'},];
 
+var getDist = function(x,y,xx,yy){ //get dist between point x,y and point xx,yy (I'm actually using high school math in real life!)
+	return Math.sqrt( Math.pow((Math.abs(x - xx)),2) + Math.pow((Math.abs(y - yy)),2) );
+}
+var getAngle = function(x,y,xx,yy){//SOH CAH TOA!
+	return Math.atan(Math.abs(y - yy)/Math.abs(x - xx));
+}
+
 var drawCirc = function(circ){with(circ){
 	game.beginPath();
 	game.arc(x,y,r,0,2*Math.PI,true);
@@ -39,13 +46,13 @@ var drawCirc = function(circ){with(circ){
 	game.stroke();
 }}
 
-var collide = function(obj1,xx,yy,xDir,yDir){with(obj1){
+var collide = function(obj1,xx,yy,xDir,yDir,offset){with(obj1){
 	if(xx != null){
-		x = xx + xDir*(r+w/2);
+		(offset) ? x = xx + xDir*(r+w/2) : x = xx + xDir;
 		xForce -= xForce*1.5;
 		}
 	if(yy != null){
-		y = yy + yDir*(r+w/2);
+		(offset) ? y = yy + yDir*(r+w/2) : y = yy + yDir;
 		yForce -= yForce*1.5;
 	}
 }}
@@ -60,18 +67,29 @@ var moveCirc = function(circ){with(circ){
 	x += xForce*.9;
 	(yForce == gravity) ? yForce = gravity : yForce += r/1000 * lag;
 	//colission with stage
-	if(y + r + w/2 >= stage.bottom){collide(circ,null,stage.bottom,0,-1)}
-	if(y - r - w/2 <= stage.top){collide(circ,null,stage.top,0,1)}
-	if(x + r + w/2 >= stage.right){collide(circ,stage.right,null,-1,0)}
-	if(x - r - w/2 <= stage.left){collide(circ,stage.left,null,1,0)}
+	if(y + r + w/2 >= stage.bottom){collide(circ,null,stage.bottom,0,-1,true)}
+	if(y - r - w/2 <= stage.top){collide(circ,null,stage.top,0,1,true)}
+	if(x + r + w/2 >= stage.right){collide(circ,stage.right,null,-1,0,true)}
+	if(x - r - w/2 <= stage.left){collide(circ,stage.left,null,1,0,true)}
 	//Colission with balls
 	for(var i=0;i<circles.length;i++){
-		var distance = (Math.abs(x - circles[i].x)+Math.abs(y - circles[i].y))/2;
+		var dist = getDist(x,y,circles[i].x,circles[i].y);
 		var sizes = r + w/2 + circles[i].r + circles[i].w/2;
-		if(out==circles[i].out && distance <= sizes && distance != 0){
-			var offx = Math.abs(x - circles[i].x);
-			var offy = Math.abs(y - circles[i].y);
-			console.log('collide',distance,sizes);
+		if(out==circles[i].out && dist <= sizes && dist != 0){
+			var offx = x - circles[i].x;
+			var offy = y - circles[i].y;
+			var xDir = (offx>0) ? 1 : -1;
+			var yDir = (offy>0) ? 1 : -1;
+			var rS = r/circles[i].r; //ratio between radii, circ being on top
+			// yForce -= circles[i].yForce;
+			// xForce -= circles[i].xForce;
+			// circles[i].yForce -= yForce;
+			// circles[i].xForce -= xForce;
+			collide(circ, x,y, xDir, yDir,false);
+			collide(circles[i], circles[i].x,circles[i].y, -xDir, -yDir,false);
+
+
+			// console.log(x,y);
 		}
 	}
 }}
@@ -93,7 +111,7 @@ window.addEventListener('mousedown', function(e){
 	var xx = e.clientX - Canvas.getBoundingClientRect().left;
 	var yy = e.clientY - Canvas.getBoundingClientRect().top;
 	for(var i=0;i<circles.length;i++){with(circles[i]){
-		if(Math.abs(xx - x) <= r + w/2 && Math.abs(yy - y) <= r + w/2){
+		if(getDist(x,y,xx,yy) <= r + w/2){
 			input.click = true;
 			input.selected = i;
 		}
@@ -105,8 +123,8 @@ window.addEventListener('mousemove', function(e){
 		var xx = e.clientX - Canvas.getBoundingClientRect().left;
 		var yy = e.clientY - Canvas.getBoundingClientRect().top;
 		with(circles[input.selected]){
-			if(Math.abs(xx - x) <= r + w/2 && Math.abs(yy - y) <= r + w/2){
-				var scale = (Math.abs(xx-x) + Math.abs(yy-y))/2; //how far mouse from radius
+			if(getDist(x,y,xx,yy) <= r + w/2){
+				var scale = getDist(x,y,xx,yy); //how far mouse from radius
 				w = (r - scale + w/2)/2;
 				xForce = 0;
 				yForce = 0;
@@ -127,6 +145,7 @@ window.addEventListener('mouseup', function(e){
 	input.selected = null;
 },false);
 circles.push(new newCircle(30,50,0));
+console.log(getAngle(2,4,7,14));
 circles.push(new newCircle(ran(stage.left,stage.right),ran(stage.top,stage.bottom),0));
 circles.push(new newCircle(ran(stage.left,stage.right),ran(stage.top,stage.bottom),0));
 circles.push(new newCircle(ran(stage.left,stage.right),ran(stage.top,stage.bottom),0));
